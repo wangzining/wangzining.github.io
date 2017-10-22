7cbceb840262aadde19188ee03a078664e7eb51f
Ning <ning@Ningss-MacBook-Pro.local>
2017-10-22 02:38:48 -0700

    Update LinkedIn

--- JS files/about.js
+++ JS files/about.js
@@ -1,3 +1,13 @@
 function displayLinkin() {
-    document.getElementById("LinkedinWebpage").innerHTML = "https://www.linkedin.com/in/wznwzn/";
+	var link = document.createElement('k');
+    //document.getElementById("LinkedinWebpage").innerHTML = "https://www.linkedin.com/in/wangzining43/";
+    link.innerHTML = "https://www.linkedin.com/in/wangzining43/";
+    link.href = "https://www.linkedin.com/in/wangzining43/";
+    document.getElementById("LinkedinWebpage").appendChild(link);
+    
+	// var linkText = document.createTextNode("my title text");
+	// link.appendChild(linkText);
+	// link.title = "my title text";
+	// link.href = "https://www.linkedin.com/in/wangzining43/";
+	// document.body.appendChild(lik);
 }
