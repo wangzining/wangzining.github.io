3b2bea28fed736c21c705427026b5e1ab7e87a4a
Ning <ning@Ningss-MacBook-Pro.local>
2018-01-29 04:20:53 -0800

    Fix bugs

--- JS files/about.js
+++ JS files/about.js
@@ -1,9 +1,14 @@
+var count = 0;
 function displayLinkin() {
 	var link = document.createElement('a');
     //document.getElementById("LinkedinWebpage").innerHTML = "https://www.linkedin.com/in/wangzining43/";
     link.innerHTML = "https://www.linkedin.com/in/wangzining43/";
     link.href = "https://www.linkedin.com/in/wangzining43/";
-    document.getElementById("LinkedinWebpage").appendChild(link);
+    if(count == 0){
+    	document.getElementById("LinkedinWebpage").appendChild(link);
+    	count = 1;
+    }
+    
     
 	// var linkText = document.createTextNode("my title text");
 	// link.appendChild(linkText);
